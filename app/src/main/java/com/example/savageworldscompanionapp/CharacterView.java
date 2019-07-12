ec29a0bf634a03fc0a4192b9011252c9c0595949
CodyGardner <cody.gardner@ucdenver.edu>
2019-07-11 21:48:35 -0600

    Added empty activities for Character Selection, Character Creation, Character View, and Dice Roller

--- app/src/main/java/com/example/savageworldscompanionapp/CharacterView.java
+++ app/src/main/java/com/example/savageworldscompanionapp/CharacterView.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CharacterSelection extends AppCompatActivity {
+public class CharacterView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_character_selection);
+        setContentView(R.layout.activity_character_view);
     }
 }
